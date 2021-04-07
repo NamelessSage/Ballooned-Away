0891420b3baa4089ad00c1761d4ad47d6f23518f
Xricore <61002997+Xricore@users.noreply.github.com>
2021-04-07 15:19:34 +0300

    update

---
+++ Assets/Scripts/PickupShroom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupShroom : MonoBehaviour
+{
+    private GameObject shroom;
+    private void Start()
+    {
+        shroom = transform.gameObject;
+    }
+}
