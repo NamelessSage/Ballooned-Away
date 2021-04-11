543f471e69641da29e8900b32b07a5e78062b77f
Xricore <61002997+Xricore@users.noreply.github.com>
2021-04-11 16:21:45 +0300

    ok

---
+++ Assets/Scripts/Bush.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bush : MonoBehaviour
+{
+    private GameObject game;
+    public int force = 100;
+    private Animator animator;
+    private bool push = false;
+    private void Start()
+    {
+        game = transform.gameObject;
+        animator = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.CompareTag("Bush"))
+        {
+            if (other.CompareTag("Player"))
+            {
+                if (push == false)
+                {
+                    animator.Play("Bushpush");
+                    StartCoroutine(Push());
+                    
+                }
+            }
+        }
+    }
+
+    private IEnumerator Push()
+    {
+        push = true;
+        Collider[] collider = Physics.OverlapSphere(transform.position, 5);
+        foreach (Collider temp in collider)
+        {
+            Rigidbody rb = temp.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force,transform.position,5);
+            }
+        }
+        yield return new WaitForSeconds(2f);
+        push = false;
+    }
+}
