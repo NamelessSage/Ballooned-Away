58b80b1943776ff909846903beeb04ebab1de69f
Xricore <61002997+Xricore@users.noreply.github.com>
2021-05-05 15:18:58 +0300

    Health update

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetHealth(int temp)
+    {
+        slider.value = temp;
+    }
+    public void SetMaxHealth(int temp)
+    {
+        slider.maxValue = temp;
+        slider.value = temp;
+    }
+}
