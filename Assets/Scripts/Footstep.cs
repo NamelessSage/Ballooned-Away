0bfe5c166f53d8ddb6c3debd8ceda7dc79da21fb
Xricore <61002997+Xricore@users.noreply.github.com>
2021-05-10 12:45:37 +0300

    Walking sounds

---
+++ Assets/Scripts/Footstep.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footstep : MonoBehaviour
+{
+
+    public AudioSource audio;
+    public AudioSource audiograss;
+    
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Rock"))
+        {
+            audio.Play();
+        }
+        if (other.collider.CompareTag("Grass"))
+        {
+            audiograss.Play();
+        }
+    }
+ 
+}
