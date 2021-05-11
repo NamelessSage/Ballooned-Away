2e6a79fe4743616d5127008469b3de8694929e27
Remigjus Buda <remyga39@gmail.com>
2021-05-11 11:26:04 +0300

    Gynimui

---
+++ Assets/Scripts/Enemy/Wall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class Wall : MonoBehaviour
+    {
+        public int Health = 3;
+        private bool dead = false;
+
+        private void Update()
+        {
+            if (Health <= 0 && !dead)
+            {
+                dead = true;
+                StartCoroutine(breaker());
+               
+            }
+        }
+        private IEnumerator breaker()
+        {
+            yield return new WaitForSeconds(1);
+            Destroy(gameObject);
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Projectile"))
+            {
+                Health--;
+            }
+        }
+        
+
+    }
+}
